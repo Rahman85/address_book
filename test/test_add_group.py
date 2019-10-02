50300d49c1a12846f0cc48db0983c01e4d782399
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-02 09:55:03 -0400

    login and logout trasfered to conftest

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,13 +2,12 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.create(Group(name="hfjhgjhghjgj", header="jhgjgghjgj", footer="hjjgjhgj"))
-    app.session.logout()
+
 
 
 def test_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
 
