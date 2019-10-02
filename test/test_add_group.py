0e62231b63c15740407570e812713c5bd1300173
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-02 10:28:07 -0400

    ensure_logged_in added

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,11 +2,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-
     app.group.create(Group(name="hfjhgjhghjgj", header="jhgjgghjgj", footer="hjjgjhgj"))
-
-
-
+ 
 def test_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
