945737551766d18e67a9c65ed7c880881eb4a91c
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-02 23:49:28 -0400

    randomization for del and modi groups added

--- fixture/group.py
+++ fixture/group.py
@@ -38,8 +38,13 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
+        self.delete_group_by_index(0)
+
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group(wd)
+        self.select_group_by_index(index)
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
@@ -48,10 +53,24 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group(wd)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def modify_group_by_index(self, index, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
