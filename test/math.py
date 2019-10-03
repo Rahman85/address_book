eee1089e2a5b4276d77af1b5f36af4780b363feb
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-02 23:18:15 -0400

    cache is added

--- test/math.py
+++ test/math.py
@@ -1,3 +1,8 @@
+total = 0
 
-
-
+for x in range (11,24):
+    if x%2!=0:
+        print(x)
+    total +=x
+print(total)
+print(51*99)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,9 +5,13 @@ def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New name"))
+    group = Group(name="New name")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
 
 
 
@@ -15,9 +19,13 @@ def test_modify_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New header"))
+    group = Group(header="New header")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
 
 
 
