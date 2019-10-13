36a6408ffc1e9504b4918ab59df127a1dafdebf0
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-12 23:04:38 -0400

    test db matches ui is added

--- test/conftest.py
+++ test/conftest.py
@@ -6,11 +6,8 @@ import importlib
 import json
 from fixture.db import DbFixture
 
-
-
 fixture = None
 target = None
-
 
 
 def load_config(file):
@@ -22,40 +19,44 @@ def load_config(file):
     return target
 
 
-
-@pytest.fixture #(scope = "session")
+@pytest.fixture  # (scope = "session")
 def app(request):
     global fixture
-
     browser = request.config.getoption("--browser")
     web_config = load_config(request.config.getoption("--target"))["web"]
-    if fixture is None or not fixture.is_valid() :
+    if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
-@pytest.fixture (scope="session")
+
+@pytest.fixture(scope="session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))["db"]
-    dbfixture =DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config[""])
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
+                          password=db_config['password'])
+
     def fin():
         dbfixture.destroy()
+
     request.addfinalizer(fin)
     return dbfixture
 
 
-
-@pytest.fixture (scope="session", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
+
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
+
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
@@ -70,7 +71,7 @@ def pytest_generate_tests(metafunc):
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
+
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
-
