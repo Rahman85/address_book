ec8ecd42b483cea7f2f7aca95a25f21c0f8e5c64
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-14 23:26:24 -0400

    db for contacts added

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -27,7 +27,7 @@ def test_delete_some_group(app):
     assert old_groups == new_groups
 
 
-def test_delete_some_group_db_assertion(app, db):
+def test_delete_some_group_db_assertion(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -37,3 +37,7 @@ def test_delete_some_group_db_assertion(app, db):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
