66fff2b076d09c845fe3df47d289de5968c209c7
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2019-10-17 09:08:02 -0400

    query for groups and contacts created

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,5 +1,6 @@
 from fixture.db import DbFixture
 from fixture.orm import ORMFixture
+from model.group import Group
 
 
 #db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
@@ -19,13 +20,24 @@ try:
     groups = db.get_group_list()
     for group in groups:
         print(group)
-    print(len(groups))'''
+    print(len(groups))
 
     contacts = db.get_contact_list()
     for contact in contacts:
         print(contact)
     print(len(contacts))
 
+    l = db.get_contacts_in_group(Group(id="300"))
+    for item in l:
+        print(item)
+    print(len(l))'''
+
+    l = db.get_contacts_not_in_group(Group(id="300"))
+    for item in l:
+        print(item)
+    print(len(l))
+
+
 finally:
-    pass#db.destroy()
+    pass #db.destroy
 
